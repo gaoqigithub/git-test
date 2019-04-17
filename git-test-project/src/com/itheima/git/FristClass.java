281b06bad4ff78cbd65f24b8bef70d1354774a59
gaoqi <1137092791@qq.com>
2019-04-17 18:13:55 +0800

    test

--- git-test-project/src/com/itheima/git/FristClass.java
+++ git-test-project/src/com/itheima/git/FristClass.java
@@ -9,4 +9,10 @@ public class FristClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i=0;
+        i+=10;
+    }
+
 }
